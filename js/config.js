9314ede2c2432bcc909b6bcae0053a0db2a11f6e
Léo Oliveira (Omni) <leonardo.oliveira3681@gmail.com>
2026-07-09 14:08:33 -0300

    Update Supabase configuration values

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
 // (Project Settings → API)
 // =========================================================
 
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://wdyvnczubupwkjkvlvgg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkeXZuY3p1YnVwd2tqa3ZsdmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDkwMzcsImV4cCI6MjA5OTE4NTAzN30._pO3_gVX-bkqLA9_UkdcPYTbWV3IH_G-8XG93-3vQFA";
 
 // Cliente Supabase global (usa CDN incluída no HTML)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
